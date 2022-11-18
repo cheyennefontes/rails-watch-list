da650c56957a47b01ced750f577b36162c1a464e
Cheyenne Fontes <cheyennefontes@hotmail.com>
2022-11-18 12:20:51 -0300

    feito

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
+  get 'bookmarks/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :show, :create, :new]  do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,15 @@
 require 'open-uri'
 require 'json'
 
-puts "Cleaning up database..."
+url = 'https://tmdb.lewagon.com/movie/top_rated'
 Movie.destroy_all
-puts "Database cleaned"
-
-url = "http://tmdb.lewagon.com/movie/top_rated"
 10.times do |i|
-  puts "Importing movies from page #{i + 1}"
-  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
-  movies.each do |movie|
-    puts "Creating #{movie['title']}"
-    base_poster_url = "https://image.tmdb.org/t/p/original"
-    Movie.create(
-      title: movie['title'],
-      overview: movie['overview'],
-      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
-      rating: movie['vote_average']
-    )
-  end
+  puts "importing #{i + 1} films from API"
+  movies = JSON.parse(URI.open("#{url}").read)
+  movie_hash = movies['results'][i]
+  hash = { title: movie_hash['original_title'], overview: movie_hash['overview'], poster_url: "https://image.tmdb.org/t/p/w300/#{movie_hash["poster_path"]}", rating: movie_hash['vote_average'] }
+  Movie.create(hash)
+  puts "#{i + 1} movie created"
 end
-puts "Movies created"
+
+puts 'Seeds completed'
