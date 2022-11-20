d56b44af3cda6891157af91f579c01a551e9f7a7
Cheyenne Fontes <cheyennefontes@hotmail.com>
2022-11-20 16:34:02 -0300

    finalmente (upload img completed

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: %i[ show edit update destroy ]
+  before_action :set_list, only: [:show, :destroy]
   def index
     @lists = List.all
   end
@@ -25,7 +25,7 @@ class ListsController < ApplicationController
 
   def destroy
     @list.destroy
-    redirect_to lists_path, notice: "List was successfully destroyed."
+    redirect_to lists_path, status: :see_other
   end
 
   private
@@ -33,7 +33,6 @@ class ListsController < ApplicationController
   def set_list
     @list = List.find(params[:id])
   end
-
 
   def list_params
     params.require(:list).permit(:name, :photo)
