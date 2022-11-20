cadbf741db346ead38c3276ed8f38eaba7ca8225
Cheyenne Fontes <cheyennefontes@hotmail.com>
2022-11-20 02:21:13 -0300

    cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+
+  Cloudinary .config do |config|
+    config.cloud_name = "dcfii4sfc"
+    config.api_key = "849982254611176"
+    config.api_secret = "SHCHhFO6XC2mbJQpmFNf5gFKqE4"
+    config.secure = true
+  end
