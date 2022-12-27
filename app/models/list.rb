ca3c6bdc92a3bc79c69c15bdbd9ad735ed75ad87
Cheyenne Fontes <cheyennefontes@hotmail.com>
2022-12-27 17:14:12 -0300

    adc filmes banco de dados

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,6 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
-  has_many :comments, dependent: :destroy
   validates :name, uniqueness: true
   validates :name, presence: true
   has_one_attached :photo
